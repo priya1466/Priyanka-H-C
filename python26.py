9d4935a805ea1b2199bc92b7ea996796d68bc24e
priya1466 <priyanka.h.cgowda2003@gmail.com>
2024-12-28 10:10:53 +0530

    Create python26.py

---
+++ python26.py
@@ -0,0 +1,8 @@
+for row in range(5):
+    for col in range(row+1):
+        print (row,end=" ")
+    print()
+for row in range(5):
+    for col in range(row+1):
+        print (col,end=" ")
+    print()
