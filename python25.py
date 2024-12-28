3459bdbb301272af5a085cd0ba346fa1afe81e45
priya1466 <priyanka.h.cgowda2003@gmail.com>
2024-12-28 10:05:50 +0530

    Create python25.py

---
+++ python25.py
@@ -0,0 +1,8 @@
+for row in range(5):
+    for col in range(row-1):
+        print ("*",end=" ")
+    print()
+for row in range(4):
+    for col in range(4-row):
+        print ("*",end=" ")
+    print()
