834888bfe6d1f58b60e2eb7204fbc5eba4208424
priya1466 <priyanka.h.cgowda2003@gmail.com>
2025-01-17 12:11:21 +0530

    Create python practice2.py

---
+++ python practice2.py
@@ -0,0 +1,6 @@
+username= input("enter your username:")
+password= input("enter your passwoed:")
+if username=="priyanka" and password=="1235":
+    print("passwoed is successfully locked in!")
+else:
+    print("invalid username or password.")
