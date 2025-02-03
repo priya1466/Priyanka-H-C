feb83200202fb05a0979bfad9701f3ed38a4e82e
priya1466 <priyanka.h.cgowda2003@gmail.com>
2025-02-03 08:45:09 +0530

    Create python89.py

---
+++ python88/python89.py
@@ -0,0 +1,16 @@
+def div():
+    try:
+        a=int(input('enter number:'))
+        b=int(input('enter number:'))
+        c=a/b
+        print(c)
+    except ZeroDivisionError:
+        print('cannot divide by zero')
+div()
+def add():
+     e=int(input('enter number:')) 
+     f=int(input('enter number:'))
+     r=e+f
+     print(r) 
+add() 
+        
